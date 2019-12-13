348b8023382db1908e0df3fcf56a9a12b242a32c
itsagitname <smtjn9026@gmail.com>
2019-12-13 22:32:20 +0530

    first prototype commit

---
+++ src/graphql/queries.js
@@ -0,0 +1,61 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const syncNotes = `query SyncNotes(
+  $filter: ModelNoteFilterInput
+  $limit: Int
+  $nextToken: String
+  $lastSync: AWSTimestamp
+) {
+  syncNotes(
+    filter: $filter
+    limit: $limit
+    nextToken: $nextToken
+    lastSync: $lastSync
+  ) {
+    items {
+      id
+      name
+      description
+      status
+      _version
+      _deleted
+      _lastChangedAt
+    }
+    nextToken
+    startedAt
+  }
+}
+`;
+export const getNote = `query GetNote($id: ID!) {
+  getNote(id: $id) {
+    id
+    name
+    description
+    status
+    _version
+    _deleted
+    _lastChangedAt
+  }
+}
+`;
+export const listNotes = `query ListNotes(
+  $filter: ModelNoteFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      name
+      description
+      status
+      _version
+      _deleted
+      _lastChangedAt
+    }
+    nextToken
+    startedAt
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = `subscription OnCreateNote {
+  onCreateNote {
+    id
+    name
+    description
+    status
+    _version
+    _deleted
+    _lastChangedAt
+  }
+}
+`;
+export const onUpdateNote = `subscription OnUpdateNote {
+  onUpdateNote {
+    id
+    name
+    description
+    status
+    _version
+    _deleted
+    _lastChangedAt
+  }
+}
+`;
+export const onDeleteNote = `subscription OnDeleteNote {
+  onDeleteNote {
+    id
+    name
+    description
+    status
+    _version
+    _deleted
+    _lastChangedAt
+  }
+}
+`;
